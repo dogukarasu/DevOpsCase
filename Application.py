03186bff220abbfe5828171bf7ea0fa3acd17be0
Doğukan Karasu <106231703+dogukarasu@users.noreply.github.com>
2022-06-07 09:25:43 +0300

    Application.py cahanged

--- Application.py
+++ Application.py
@@ -16,7 +16,7 @@ class Storage():
   def __init__(self):
     self.db = mysql.connector.connect(
       user   = os.getenv('MYSQL_USERNAME'),
-      passwd = os.getenv('MYSQL_PASSWORD'),
+      password = os.getenv('MYSQL_PASSWORD'),
       db     = os.getenv('MYSQL_INSTANCE_NAME'),
       host   = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
       port   = os.getenv('MYSQL_PORT_3306_TCP_PORT')
